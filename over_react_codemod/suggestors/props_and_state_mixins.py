77ed53730f3eaf0cc03ac1c2ec86db70ddb15ab2
evanweible-wf <evan.weible@workiva.com>
2018-12-18 10:07:23 -0700

    Add OSS license header to all source files.

--- over_react_codemod/suggestors/props_and_state_mixins.py
+++ over_react_codemod/suggestors/props_and_state_mixins.py
@@ -1,3 +1,17 @@
+# Copyright 2018 Workiva Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from ..regexes import CLASS_BODY_BRACES_REGEX, PROPS_OR_STATE_MIXIN_REGEX, PROPS_OR_STATE_MIXIN_ANNOTATION_REGEX, CLASS_DECLARATION_REGEX
 from ..updaters import insert_props_or_state_meta, rename_props_or_state_mixin
 from ..util import get_props_or_state_meta_const, get_meta_type, eprint, get_meta_const_ignore_line
