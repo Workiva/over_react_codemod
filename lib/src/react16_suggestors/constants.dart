f82d918975da8cc5f5bb2982e6eb394c6d4fd436
Joseph Bingham <joe.bingham@workiva.com>
2019-07-31 13:06:52 -0700

    Format

--- lib/src/react16_suggestors/constants.dart
+++ lib/src/react16_suggestors/constants.dart
@@ -1,7 +1,6 @@
 const fixmePrefix = 'FIXME:over_react_codemod';
 
-const manualValidationCommentSubstring =
-    'Check this box upon manual';
+const manualValidationCommentSubstring = 'Check this box upon manual';
 
 const willBeRemovedCommentSuffix =
     ' This will be removed once the transition to React 16 is complete.';
