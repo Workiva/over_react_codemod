24be5a6f436929820dd807ce61547d130c6cf4a9
Joseph Bingham <joe.bingham@workiva.com>
2019-08-02 14:39:52 -0700

    Add Greg's suggestions

---
+++ lib/src/react16_suggestors/react16_utilities.dart
@@ -0,0 +1,41 @@
+import 'package:analyzer/analyzer.dart';
+import 'package:analyzer/dart/ast/token.dart';
+import 'package:source_span/source_span.dart';
+
+import 'constants.dart';
+
+bool hasValidationComment(AstNode node, SourceFile sourceFile) {
+  final line = sourceFile.getLine(node.offset);
+
+  // Find the comment associated with this line; doesn't work with visitor for some reason.
+  String commentText;
+  for (var comment in allComments(node.root.beginToken)) {
+    final commentLine = sourceFile.getLine(comment.end);
+
+    if (commentLine == line || commentLine == line + 1) {
+      commentText = sourceFile.getText(comment.offset, comment.end);
+      break;
+    }
+  }
+
+  return commentText?.contains(manualValidationCommentSubstring) ?? false;
+}
+
+/// Returns an iterable of all the comments from [beginToken] to the end of the
+/// file.
+///
+/// Comments are part of the normal stream, and need to be accessed via
+/// [Token.precedingComments], so it's difficult to iterate over them without
+/// this method.
+Iterable allComments(Token beginToken) sync* {
+  var currentToken = beginToken;
+  while (!currentToken.isEof) {
+    var currentComment = currentToken.precedingComments;
+    while (currentComment != null) {
+      yield currentComment;
+      currentComment = currentComment.next;
+    }
+    currentToken = currentToken.next;
+  }
+  ;
+}
