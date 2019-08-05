98f1647ec83124f748dfc15bf61ca2f17044b6b7
Sydney Jodon <sydney.jodon@workiva.com>
2019-08-05 15:43:41 -0700

    Add tests

---
+++ test/component2_suggestors/copyunconsumeddomprops_migrator_test.dart
@@ -0,0 +1,95 @@
+// Copyright 2019 Workiva Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import 'package:over_react_codemod/src/component2_suggestors/copyunconsumeddomprops_migrator.dart';
+import 'package:test/test.dart';
+
+import '../util.dart';
+
+main() {
+  group('CopyUnconsumedDomPropsMigrator', () {
+    final testSuggestor = getSuggestorTester(CopyUnconsumedDomPropsMigrator());
+
+//    test('empty file', () {
+//      testSuggestor(expectedPatchCount: 0, input: '');
+//    });
+//
+//    test('no matches', () {
+//      testSuggestor(
+//        expectedPatchCount: 0,
+//        input: '''
+//          library foo;
+//          var a = 'b';
+//          class Foo {}
+//        ''',
+//      );
+//    });
+
+    test('copyUnconsumedDomProps updates', () {
+      testSuggestor(
+        expectedPatchCount: 2,
+        input: '''
+          @Component2()
+          class FooComponent extends UiComponent2<FooProps> {
+            @override
+            render() {
+              return (Dom.span()
+                ..addProps(copyUnconsumedDomProps())
+              )(props.children);
+            }
+          }
+        ''',
+        expectedOutput: '''
+          @Component2()
+          class FooComponent extends UiComponent2<FooProps> {
+            @override
+            render() {
+              return (Dom.span()
+                ..modifyProps(addUnconsumedDomProps)
+              )(props.children);
+            }
+          }
+        ''',
+      );
+    });
+
+    test('copyUnconsumedProps updates', () {
+      testSuggestor(
+        expectedPatchCount: 2,
+        input: '''
+          @Component2()
+          class FooComponent extends UiComponent2<FooProps> {
+            @override
+            render() {
+              return (Dom.span()
+                ..addProps(copyUnconsumedProps())
+              )(props.children);
+            }
+          }
+        ''',
+        expectedOutput: '''
+          @Component2()
+          class FooComponent extends UiComponent2<FooProps> {
+            @override
+            render() {
+              return (Dom.span()
+                ..modifyProps(addUnconsumedProps)
+              )(props.children);
+            }
+          }
+        ''',
+      );
+    });
+  });
+}
