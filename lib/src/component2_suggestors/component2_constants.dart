0c3ffdc5093c8fc45b81dc5162850d0852cc9318
Joseph Bingham <joe.bingham@workiva.com>
2019-08-06 10:15:21 -0700

    Update comment wording

--- lib/src/component2_suggestors/component2_constants.dart
+++ lib/src/component2_suggestors/component2_constants.dart
@@ -2,8 +2,9 @@ const revertInstructions = '/// These updates are opt-in and can be '
     'rolled back if you do not wish to do the update at this time.';
 
 String methodDeprecationMessage({methodName}) =>
-    '/// FIXME: $methodName has been '
-    'deprecated and should be updated to [unsafe_$methodName]';
+    '/// FIXME: [$methodName] has been '
+    'deprecated and should be updated to [unsafe_$methodName]. Please see '
+    'the doc comment for [$methodName] for more details on updating.';
 
 String getComponentWillUpdateComment() => '''
   ${methodDeprecationMessage(methodName: 'componentWillUpdate')}
