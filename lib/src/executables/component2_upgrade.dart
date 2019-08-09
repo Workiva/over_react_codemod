6963b05cc015538b7c607a3735b4504e22adb095
Joseph Bingham <joe.bingham@workiva.com>
2019-08-09 08:48:35 -0700

    Remove merge comments

--- lib/src/executables/component2_upgrade.dart
+++ lib/src/executables/component2_upgrade.dart
@@ -18,11 +18,8 @@ import 'package:codemod/codemod.dart';
 import 'package:over_react_codemod/src/component2_suggestors/class_name_and_annotation_migrator.dart';
 import 'package:over_react_codemod/src/component2_suggestors/componentdidupdate_migrator.dart';
 import 'package:over_react_codemod/src/component2_suggestors/componentwillmount_migrator.dart';
-<<<<<<< HEAD
 import 'package:over_react_codemod/src/component2_suggestors/deprecated_lifecycle_suggestor.dart';
-=======
 import 'package:over_react_codemod/src/component2_suggestors/copyunconsumeddomprops_migrator.dart';
->>>>>>> b364ab73468dbae4ca8720a54a94613d1ec70f00
 
 const _changesRequiredOutput = """
 To update your code, switch to Dart 2.1.0 and run the following commands:
