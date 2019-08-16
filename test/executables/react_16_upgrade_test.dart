c036bbcb58c8d794a1619e02476c20bd7ece84c1
Sydney Jodon <sydney.jodon@workiva.com>
2019-08-16 11:10:17 -0700

    Fix failing test

--- test/executables/react_16_upgrade_test.dart
+++ test/executables/react_16_upgrade_test.dart
@@ -20,7 +20,8 @@ import 'package:test/test.dart';
 ProcessResult runUpgrade({String onDirectory}) {
   Process.runSync('pub', ['get'], workingDirectory: onDirectory);
 
-  return Process.runSync('pub', ['run', 'over_react_codemod:react_16_upgrade'],
+  return Process.runSync(
+      'pub', ['global', 'run', 'over_react_codemod:react_16_upgrade'],
       workingDirectory: onDirectory);
 }
 
@@ -36,16 +37,16 @@ main() {
     group('exits with a status of 0 when', () {
       test('there is no comment', () {
         final result = runUpgrade(
-            onDirectory: 'test/executables/test_component'
-                's/file_with_no_comment');
+            onDirectory:
+                'test/executables/test_components/file_with_no_comment');
 
         expect(result.exitCode, equals(0));
       });
 
       test('there is a validated comment', () {
         final result = runUpgrade(
-            onDirectory: 'test/executables/test_component'
-                's/file_with_validated_comment');
+            onDirectory:
+                'test/executables/test_components/file_with_validated_comment');
 
         expect(result.exitCode, equals(0));
       });
