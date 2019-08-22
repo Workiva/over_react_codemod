5927a0da36f2af386a98fe7049bd0aa75f306778
Keal Jones <keal.jones@workiva.com>
2019-08-22 14:24:15 -0700

    Add yieldPatch for over_react import if not found.

--- lib/src/react16_suggestors/react_dom_render_migrator.dart
+++ lib/src/react16_suggestors/react_dom_render_migrator.dart
@@ -32,6 +32,12 @@ class ReactDomRenderMigrator extends GeneralizingAstVisitor
       return ancestor is CompilationUnit;
     });
 
+    ImportDirective overReactImport = importList.directives.lastWhere(
+        (dir) =>
+            dir is ImportDirective &&
+            dir.uri?.stringValue == 'package:over_react/over_react.dart',
+        orElse: () => null);
+
     ImportDirective reactDomImport = importList.directives.lastWhere(
         (dir) =>
             dir is ImportDirective &&
@@ -65,8 +71,19 @@ class ReactDomRenderMigrator extends GeneralizingAstVisitor
       return ancestor is FunctionDeclaration;
     });
 
+    void addOverReactPatch(int offset){
+      yieldPatch(
+        reactDomImport.offset,
+        reactDomImport.offset,
+        'import \'package:over_react/over_react.dart\';\n'
+      );
+    }
+
     // Wrap render in ErrorBoundary.
     if (!renderFirstArg.toSource().startsWith('ErrorBoundary')) {
+      if (overReactImport == null){
+        addOverReactPatch(renderFirstArg.offset);
+      }
       yieldPatch(
         renderFirstArg.offset,
         renderFirstArg.offset,
@@ -77,6 +94,8 @@ class ReactDomRenderMigrator extends GeneralizingAstVisitor
         renderFirstArg.end,
         ')',
       );
+    } else if (renderFirstArg.toSource().startsWith('ErrorBoundary') && overReactImport == null){
+      addOverReactPatch(renderFirstArg.offset);
     }
 
     if (renderFirstArg is InvocationExpression) {
