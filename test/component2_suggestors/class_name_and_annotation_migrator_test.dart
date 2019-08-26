10de5adea114205bc6991be9c09af6dd04bad821
Sydney Jodon <sydney.jodon@workiva.com>
2019-08-26 13:21:40 -0700

    Refactor class name tests

--- test/component2_suggestors/class_name_and_annotation_migrator_test.dart
+++ test/component2_suggestors/class_name_and_annotation_migrator_test.dart
@@ -19,38 +19,51 @@ import '../util.dart';
 
 main() {
   group('ClassNameAndAnnotationMigrator', () {
-    final testSuggestor = getSuggestorTester(ClassNameAndAnnotationMigrator());
+    classNameAndAnnotationTests(allowPartialUpgrades: true);
+  });
 
-    test('empty file', () {
-      testSuggestor(expectedPatchCount: 0, input: '');
-    });
+  group('ClassNameAndAnnotationMigrator with --no-partial-upgrades flag', () {
+    classNameAndAnnotationTests(allowPartialUpgrades: false);
+  });
+}
 
-    test('no matches', () {
-      testSuggestor(
-        expectedPatchCount: 0,
-        input: '''
-          library foo;
-          var a = 'b';
-          class Foo {}
-        ''',
-      );
-    });
+void classNameAndAnnotationTests({bool allowPartialUpgrades}) {
+  final testSuggestor = getSuggestorTester(ClassNameAndAnnotationMigrator(
+      allowPartialUpgrades: allowPartialUpgrades));
 
-    test('annotation with non-based extending class updates', () {
-      testSuggestor(
-        expectedPatchCount: 1,
-        input: '''
-          @Component()
-          class FooComponent extends AbstractComponent {}
-        ''',
-        expectedOutput: '''
-          @Component2()
-          class FooComponent extends AbstractComponent {}
-        ''',
-      );
-    });
+  test('empty file', () {
+    testSuggestor(expectedPatchCount: 0, input: '');
+  });
 
-    test('annotation and extending class updates', () {
+  test('no matches', () {
+    testSuggestor(
+      expectedPatchCount: 0,
+      input: '''
+        library foo;
+        var a = 'b';
+        class Foo {}
+      ''',
+    );
+  });
+
+  test(
+      'annotation with non-based extending class '
+      '${allowPartialUpgrades ? 'updates' : 'does not update'}', () {
+    testSuggestor(
+      expectedPatchCount: allowPartialUpgrades ? 1 : 0,
+      input: '''
+        @Component()
+        class FooComponent extends AbstractComponent {}
+      ''',
+      expectedOutput: '''
+        @Component${allowPartialUpgrades ? '2' : ''}()
+        class FooComponent extends AbstractComponent {}
+      ''',
+    );
+  });
+
+  group('annotation and extending class', () {
+    test('updates when all lifecycle methods have codemods', () {
       testSuggestor(
         expectedPatchCount: 2,
         input: '''
@@ -66,9 +79,6 @@ main() {
             
             @override
             componentDidUpdate(Map prevProps, Map prevState) {}
-            
-            @override
-            componentWillUnmount() {}
           }
         ''',
         expectedOutput: '''
@@ -76,6 +86,61 @@ main() {
           class FooComponent extends UiComponent2<FooProps> {
             eventHandler() {}
             
+            @override
+            componentWillMount() {}
+
+            @override
+            render() {}
+
+            @override
+            componentDidUpdate(Map prevProps, Map prevState) {}
+          }
+        ''',
+      );
+    });
+
+    test('updates with no lifecycle methods', () {
+      testSuggestor(
+        expectedPatchCount: 2,
+        input: '''
+          @Component()
+          class FooComponent extends UiComponent<FooProps> {}
+        ''',
+        expectedOutput: '''
+          @Component2()
+          class FooComponent extends UiComponent2<FooProps> {}
+        ''',
+      );
+    });
+
+    test(
+        '${allowPartialUpgrades ? 'updates' : 'does not update'} when one or '
+        'more lifecycle method has no codemod', () {
+      testSuggestor(
+        expectedPatchCount: allowPartialUpgrades ? 2 : 0,
+        input: '''
+          @Component()
+          class FooComponent extends UiComponent<FooProps> {
+            eventHandler() {}
+
+            @override
+            componentWillMount() {}
+
+            @override
+            render() {}
+
+            @override
+            componentDidUpdate(Map prevProps, Map prevState) {}
+
+            @override
+            componentWillUnmount() {}
+          }
+        ''',
+        expectedOutput: '''
+          @Component${allowPartialUpgrades ? '2' : ''}()
+          class FooComponent extends UiComponent${allowPartialUpgrades ? '2' : ''}<FooProps> {
+            eventHandler() {}
+  
             @override
             componentWillMount() {}
             
@@ -91,10 +156,34 @@ main() {
         ''',
       );
     });
+  });
 
-    test('extending class only needs updating', () {
+  group('extending class only needs updating', () {
+    test('updates when all lifecycle methods have codemods', () {
       testSuggestor(
         expectedPatchCount: 1,
+        input: '''
+          @Component2()
+          class FooComponent extends UiStatefulComponent<FooProps, FooState> {
+            @override
+            void render() {}
+          }
+        ''',
+        expectedOutput: '''
+          @Component2()
+          class FooComponent extends UiStatefulComponent2<FooProps, FooState> {
+            @override
+            void render() {}
+          }
+        ''',
+      );
+    });
+
+    test(
+        '${allowPartialUpgrades ? 'updates' : 'does not update'} when one or '
+        'more lifecycle method has no codemod', () {
+      testSuggestor(
+        expectedPatchCount: allowPartialUpgrades ? 1 : 0,
         input: '''
           @Component2()
           class FooComponent extends UiStatefulComponent<FooProps, FooState> {
@@ -102,15 +191,41 @@ main() {
             shouldComponentUpdate() {}
             
             @override
-            render() {}
+            void render() {}
           }
         ''',
         expectedOutput: '''
           @Component2()
-          class FooComponent extends UiStatefulComponent2<FooProps, FooState> {
+          class FooComponent extends UiStatefulComponent${allowPartialUpgrades ? '2' : ''}<FooProps, FooState> {
             @override
             shouldComponentUpdate() {}
             
+            @override
+            void render() {}
+          }
+        ''',
+      );
+    });
+  });
+
+  group('annotation with args and extending class', () {
+    test('updates when all lifecycle methods have codemods', () {
+      testSuggestor(
+        expectedPatchCount: 2,
+        input: '''
+          @Component(isWrapper: true)
+          class FooComponent extends UiComponent<FooProps> {
+            eventHandler() {}
+
+            @override
+            render() {}
+          }
+        ''',
+        expectedOutput: '''
+          @Component2(isWrapper: true)
+          class FooComponent extends UiComponent2<FooProps> {
+            eventHandler() {}
+
             @override
             render() {}
           }
@@ -118,9 +233,11 @@ main() {
       );
     });
 
-    test('annotation with args and extending class updates', () {
+    test(
+        '${allowPartialUpgrades ? 'updates' : 'does not update'} when one or '
+        'more lifecycle method has no codemod', () {
       testSuggestor(
-        expectedPatchCount: 2,
+        expectedPatchCount: allowPartialUpgrades ? 2 : 0,
         input: '''
           @Component(isWrapper: true)
           class FooComponent extends UiComponent<FooProps> {
@@ -132,8 +249,8 @@ main() {
           }
         ''',
         expectedOutput: '''
-          @Component2(isWrapper: true)
-          class FooComponent extends UiComponent2<FooProps> {
+          @Component${allowPartialUpgrades ? '2' : ''}(isWrapper: true)
+          class FooComponent extends UiComponent${allowPartialUpgrades ? '2' : ''}<FooProps> {
             @override
             componentWillMount() {}
             
@@ -144,13 +261,58 @@ main() {
       );
     });
 
-    test('AbstractComponent annotation and extending stateful class updates',
+    test(
+        'is non-Component ${allowPartialUpgrades ? 'updates' : 'does not update'}',
         () {
+      testSuggestor(
+        expectedPatchCount: allowPartialUpgrades ? 1 : 0,
+        input: '''
+          @Component(isWrapper: true)
+          class FooComponent extends AbstractComponent<FooProps> {
+            @override
+            render() {}
+          }
+        ''',
+        expectedOutput: '''
+          @Component${allowPartialUpgrades ? '2' : ''}(isWrapper: true)
+          class FooComponent extends AbstractComponent<FooProps> {
+            @override
+            render() {}
+          }
+        ''',
+      );
+    });
+  });
+
+  group('AbstractComponent annotation and extending stateful class', () {
+    test('updates when all lifecycle methods have codemods', () {
       testSuggestor(
         expectedPatchCount: 2,
         input: '''
           @AbstractComponent(isWrapper: true)
-          abstract class AbstractFooComponent extends UiStatefulComponent {
+          abstract class FooComponent extends UiStatefulComponent<FooProps, FooState> {
+            @override
+            componentWillMount() {}
+          }
+        ''',
+        expectedOutput: '''
+          @AbstractComponent2(isWrapper: true)
+          abstract class FooComponent extends UiStatefulComponent2<FooProps, FooState> {
+            @override
+            componentWillMount() {}
+          }
+        ''',
+      );
+    });
+
+    test(
+        '${allowPartialUpgrades ? 'updates' : 'does not update'} when one or '
+        'more lifecycle method has no codemod', () {
+      testSuggestor(
+        expectedPatchCount: allowPartialUpgrades ? 2 : 0,
+        input: '''
+          @AbstractComponent(isWrapper: true)
+          abstract class FooComponent extends UiStatefulComponent<FooProps, FooState> {
             @override
             componentWillMount() {}
 
@@ -159,8 +321,8 @@ main() {
           }
         ''',
         expectedOutput: '''
-          @AbstractComponent2(isWrapper: true)
-          abstract class AbstractFooComponent extends UiStatefulComponent2 {
+          @AbstractComponent${allowPartialUpgrades ? '2' : ''}(isWrapper: true)
+          abstract class FooComponent extends UiStatefulComponent${allowPartialUpgrades ? '2' : ''}<FooProps, FooState> {
             @override
             componentWillMount() {}
 
@@ -171,9 +333,53 @@ main() {
       );
     });
 
-    test('extending class imported from react.dart updates', () {
+    test(
+        'is non-Component ${allowPartialUpgrades ? 'updates' : 'does not update'}',
+        () {
+      testSuggestor(
+        expectedPatchCount: allowPartialUpgrades ? 1 : 0,
+        input: '''
+          @AbstractComponent(isWrapper: true)
+          abstract class FooComponent extends AbstractStatefulComponent<FooProps, FooState> {}
+        ''',
+        expectedOutput: '''
+          @AbstractComponent${allowPartialUpgrades ? '2' : ''}(isWrapper: true)
+          abstract class FooComponent extends AbstractStatefulComponent<FooProps, FooState> {}
+        ''',
+      );
+    });
+  });
+
+  group('extending class imported from react.dart', () {
+    test('updates when all lifecycle methods have codemods', () {
       testSuggestor(
         expectedPatchCount: 2,
+        input: '''
+          import 'package:react/react.dart' as react show Component;
+          import 'package:react/react_dom.dart' as react_dom;
+        
+          class FooComponent extends react.Component {
+            @override
+            void componentDidUpdate(Map prevProps, Map prevState) {}
+          }
+        ''',
+        expectedOutput: '''
+          import 'package:react/react.dart' as react show Component, Component2;
+          import 'package:react/react_dom.dart' as react_dom;
+
+          class FooComponent extends react.Component2 {
+            @override
+            void componentDidUpdate(Map prevProps, Map prevState) {}
+          }
+        ''',
+      );
+    });
+
+    test(
+        '${allowPartialUpgrades ? 'updates' : 'does not update'} when one or '
+        'more lifecycle method has no codemod', () {
+      testSuggestor(
+        expectedPatchCount: allowPartialUpgrades ? 2 : 0,
         input: '''
           import 'package:react/react.dart' as react show Component;
           import 'package:react/react_dom.dart' as react_dom;
@@ -187,10 +393,10 @@ main() {
           }
         ''',
         expectedOutput: '''
-          import 'package:react/react.dart' as react show Component, Component2;
+          import 'package:react/react.dart' as react show Component${allowPartialUpgrades ? ', Component2' : ''};
           import 'package:react/react_dom.dart' as react_dom;
 
-          class FooComponent extends react.Component2 {
+          class FooComponent extends react.Component${allowPartialUpgrades ? '2' : ''} {
             @override
             componentDidUpdate(Map prevProps, Map prevState) {}
             
@@ -202,7 +408,7 @@ main() {
     });
 
     test(
-        'extending class imported from react.dart with different import name updates',
+        'with different import name updates when all lifecycle methods have codemods',
         () {
       testSuggestor(
         expectedPatchCount: 1,
@@ -220,26 +426,107 @@ main() {
         ''',
       );
     });
+  });
 
-    test('already updated annotation and extending class does not update', () {
+  group('react.dart import show Component', () {
+    test('updates if one or more component in the file updates', () {
       testSuggestor(
-        expectedPatchCount: 0,
+        expectedPatchCount: allowPartialUpgrades ? 5 : 3,
         input: '''
-          @Component2
-          class FooComponent extends UiComponent2 {
-            eventHandler() {}
-            
+          import "package:react/react_dom.dart" as react_dom;
+          import "package:react/react.dart" as react show Component;
+        
+          class FooComponent extends react.Component {
             @override
-            init() {}
+            componentWillReceiveProps() {}
+          }
+          
+          class FooComponent extends react.Component {
+            @override
+            componentDidUpdate(Map prevProps, Map prevState) {}
+          }
+          
+          class FooComponent extends react.Component {}
+          
+          class FooComponent extends react.Component {
+            @override
+            shouldComponentUpdate() {}
             
             @override
             render() {}
+          }
+        ''',
+        expectedOutput: '''
+          import "package:react/react_dom.dart" as react_dom;
+          import "package:react/react.dart" as react show Component, Component2;
+        
+          class FooComponent extends react.Component${allowPartialUpgrades ? '2' : ''} {
+            @override
+            componentWillReceiveProps() {}
+          }
+          
+          class FooComponent extends react.Component2 {
+            @override
+            componentDidUpdate(Map prevProps, Map prevState) {}
+          }
+          
+          class FooComponent extends react.Component2 {}
+          
+          class FooComponent extends react.Component${allowPartialUpgrades ? '2' : ''} {
+            @override
+            shouldComponentUpdate() {}
             
             @override
-            componentDidUpdate(Map prevProps, Map prevState, [snapshot]) {}
+            render() {}
           }
         ''',
       );
     });
+
+    if (!allowPartialUpgrades) {
+      test('does not update if all components in the file do not update', () {
+        testSuggestor(
+          expectedPatchCount: 0,
+          input: '''
+            import "package:react/react_dom.dart" as react_dom;
+            import "package:react/react.dart" as react show Component;
+          
+            class FooComponent extends react.Component {
+              @override
+              componentWillReceiveProps() {}
+            }
+            
+            class FooComponent extends react.Component {
+              @override
+              shouldComponentUpdate() {}
+              
+              @override
+              render() {}
+            }
+          ''',
+        );
+      });
+    }
+  });
+
+  test('already updated annotation and extending class does not update', () {
+    testSuggestor(
+      expectedPatchCount: 0,
+      input: '''
+        @Component2
+        class FooComponent extends UiComponent2 {
+          eventHandler() {}
+          
+          @override
+          init() {}
+          
+          @override
+          render() {}
+          
+          @override
+          componentDidUpdate(Map prevProps, Map prevState, [snapshot]) {}
+        }
+      ''',
+    );
   });
 }
