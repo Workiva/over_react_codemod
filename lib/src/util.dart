9cb0f27120acd9748a00dbbe9ae41f921dfb83a9
Joseph Bingham <joe.bingham@workiva.com>
2019-09-05 16:59:35 -0700

    Removed commented out code

--- lib/src/util.dart
+++ lib/src/util.dart
@@ -278,19 +278,6 @@ final _usesOverReactRegex = RegExp(
 /// constraint unintentionally.
 VersionRange generateNewVersionRange(
     VersionRange currentRange, VersionRange targetRange) {
-  String versionRange;
-
-//  if (currentRange.min > targetRange.min) {
-//    versionRange = '>=${currentRange.min.toString()}';
-//  } else {
-//    versionRange = '>=${targetRange.min.toString()}';
-//  }
-//
-//  if (targetRange.max != null) {
-//    versionRange += ' <${targetRange.max.toString()}';
-//  }
-//
-//  return VersionConstraint.parse(versionRange);
   return VersionRange(
     min:
         currentRange.min > targetRange.min ? currentRange.min : targetRange.min,
