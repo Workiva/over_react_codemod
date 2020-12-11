ce0f29cb8d8826ee1fc668f2cbbc7f4e80f9b726
Sydney Jodon <sydney.jodon@workiva.com>
2020-12-11 10:42:59 -0700

    Add dart2_9_upgrade executable

---
+++ lib/src/executables/dart2_9_upgrade.dart
@@ -0,0 +1,35 @@
+// Copyright 2020 Workiva Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import 'dart:io';
+
+import 'package:codemod/codemod.dart';
+import 'package:over_react_codemod/src/util.dart';
+
+const _changesRequiredOutput = """
+  To update your code, run the following commands in your repository:
+  pub global activate over_react_codemod
+  pub global run over_react_codemod:dart2_9_upgrade
+  pub run dart_dev format (If you format this repository).
+""";
+
+void main(List<String> args) {
+  exitCode = runInteractiveCodemodSequence(
+    allDartPathsExceptHidden(),
+    [],
+    args: args,
+    defaultYes: true,
+    changesRequiredOutput: _changesRequiredOutput,
+  );
+}
