3561d017bbd9b3b38812c1808b044669557b2fe4
Greg Littlefield <greg.littlefield@workiva.com>
2021-09-02 15:57:57 -0700

    Fix ButtonGroup migration

--- lib/src/mui_suggestors/mui_button_group_migrator.dart
+++ lib/src/mui_suggestors/mui_button_group_migrator.dart
@@ -1,10 +1,8 @@
 import 'package:over_react_codemod/src/mui_suggestors/constants.dart';
-import 'package:over_react_codemod/src/mui_suggestors/mui_button_migrator.dart';
 import 'package:over_react_codemod/src/util/component_usage.dart';
 import 'package:over_react_codemod/src/util/component_usage_migrator.dart';
 
-class MuiButtonGroupMigrator
-    with ClassSuggestor, ComponentUsageMigrator, ButtonDisplayPropsMigrator {
+class MuiButtonGroupMigrator with ClassSuggestor, ComponentUsageMigrator {
   @override
   MigrationDecision shouldMigrateUsage(FluentComponentUsage usage) =>
       usesWsdFactory(usage, 'ButtonGroup')
@@ -24,7 +22,23 @@ class MuiButtonGroupMigrator
       'isVertical': (p) {
         yieldPropPatch(p, newName: 'orientation', newRhs: "'vertical'");
       },
-      'size': migrateButtonSize,
+      'size': migrateSize,
     });
+  }
+
+  void migrateSize(PropAssignment prop) {
+    final sizeFromWsdSize = mapWsdConstant(prop.rightHandSide, const {
+      'ButtonGroupSize.XXSMALL': '$muiNs.ButtonSize.xxsmall',
+      'ButtonGroupSize.XSMALL': '$muiNs.ButtonSize.xsmall',
+      'ButtonGroupSize.SMALL': '$muiNs.ButtonSize.small',
+      'ButtonGroupSize.DEFAULT': '$muiNs.ButtonSize.medium',
+      'ButtonGroupSize.LARGE': '$muiNs.ButtonSize.large',
+    });
+    if (sizeFromWsdSize != null) {
+      yieldPropPatch(prop, newRhs: sizeFromWsdSize);
+      return;
+    }
+
+    yieldPropManualMigratePatch(prop);
   }
 }
