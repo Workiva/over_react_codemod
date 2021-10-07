13c98c2af52cf706384935c595eb5a3f87780b69
Greg Littlefield <greg.littlefield@workiva.com>
2021-10-07 14:43:14 -0700

    Fix isVertical migration to use constants and not always be 'vertical'

--- lib/src/mui_suggestors/components/mui_button_group_migrator.dart
+++ lib/src/mui_suggestors/components/mui_button_group_migrator.dart
@@ -1,3 +1,4 @@
+import 'package:analyzer/dart/ast/ast.dart';
 import 'package:over_react_codemod/src/mui_suggestors/constants.dart';
 import 'package:over_react_codemod/src/util/component_usage.dart';
 import 'package:over_react_codemod/src/util/component_usage_migrator.dart';
@@ -16,14 +17,30 @@ class MuiButtonGroupMigrator with ClassSuggestor, ComponentUsageMigrator {
     yieldPatchOverNode('$muiNs.ButtonGroup', usage.factory!);
 
     migratePropsByName(usage, migratorsByName: {
-      'isJustified': (p) {
-        yieldPropPatch(p, newName: 'fullWidth');
-      },
-      'isVertical': (p) {
-        yieldPropPatch(p, newName: 'orientation', newRhs: "'vertical'");
-      },
+      'isJustified': (p) => yieldPropPatch(p, newName: 'fullWidth'),
+      'isVertical': migrateIsVertical,
       'size': migrateSize,
     });
+  }
+
+  void migrateIsVertical(PropAssignment prop) {
+    const horizontalOrientation = "$muiNs.ButtonGroupOrientation.horizontal";
+    const verticalOrientation = "$muiNs.ButtonGroupOrientation.vertical";
+
+    final rhs = prop.rightHandSide;
+    if (rhs is BooleanLiteral) {
+      yieldPropPatch(prop,
+          newName: 'orientation',
+          newRhs: rhs.value ? verticalOrientation : horizontalOrientation);
+    } else {
+      // Change
+      //     ..isVertical = expression
+      // to
+      //     ..orientation = expression ? mui.ButtonGroupOrientation.vertical : mui.ButtonGroupOrientation.horizontal
+      yieldPropPatch(prop, newName: 'orientation');
+      yieldInsertionPatch(
+          ' ? $verticalOrientation : $horizontalOrientation', rhs.end);
+    }
   }
 
   void migrateSize(PropAssignment prop) {
