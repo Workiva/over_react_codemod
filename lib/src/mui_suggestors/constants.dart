2af65cd35f6d9acec876d30194be71e4a381ede5
Greg Littlefield <greg.littlefield@workiva.com>
2021-10-27 14:12:25 -0700

    Add doc comment to muiNs

--- lib/src/mui_suggestors/constants.dart
+++ lib/src/mui_suggestors/constants.dart
@@ -1,1 +1,6 @@
+/// The standard namespace to use when importing react_material_ui.
+///
+/// This should be used in all component codemods, since this namespace
+/// is what `muiImporter` picks up on to add the react_material_ui import to
+/// libraries only when it's needed.
 const muiNs = 'mui';
