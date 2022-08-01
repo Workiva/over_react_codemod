faff383758a2f247e252d4ce0158fc43aabaecaf
Alan Knight <alan.knight@workiva.com>
2022-08-01 12:16:22 -0400

    Handle adjacent strings, exclude strings with no alphabetics

--- lib/src/intl_suggestors/intl_migrator.dart
+++ lib/src/intl_suggestors/intl_migrator.dart
@@ -28,6 +28,7 @@ class IntlMigrator extends ComponentUsageMigrator {
 
   @override
   bool shouldMigrateUsage(FluentComponentUsage usage) =>
+      // TODO: Handle adjacent strings with an interpolation.
       usage.cascadedProps.any((prop) =>
           isValidStringLiteralProp(prop) ||
           isValidStringInterpolationProp(prop)) ||
@@ -57,7 +58,7 @@ class IntlMigrator extends ComponentUsageMigrator {
     final childNodes = usage.children.map((child) => child.node);
     //Migrate String Literals
     childNodes
-        .whereType<SimpleStringLiteral>()
+        .whereType<StringLiteral>()
         .forEach((node) => migrateChildStringLiteral(node));
 
     //Migrate String Interpolations
@@ -92,7 +93,7 @@ class IntlMigrator extends ComponentUsageMigrator {
   }
 
   void migrateChildStringLiteral(
-    SimpleStringLiteral node,
+    StringLiteral node,
   ) {
     if (isValidStringLiteralNode(node)) {
       final functionCall = intlStringAccess(node, _className);
