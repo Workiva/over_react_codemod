65951475d0b7cc674766a6f7014aa2525fc9350f
Alan Knight <alan.knight@workiva.com>
2022-11-07 09:24:51 -0500

    Update executable tests

--- test/executables/intl_message_migration_test.dart
+++ test/executables/intl_message_migration_test.dart
@@ -264,7 +264,7 @@ usage() => (mui.Button()..aria.label=TestProjectIntl.sortsLater)(TestProjectIntl
       d.dir('src', [
         d.dir('intl', [
           d.file('test_project_intl.dart', /*language=dart*/ '''
-import 'package:intl/intl.dart';
+import 'package:w_intl/intl_wrapper.dart';
 
 //ignore: avoid_classes_with_only_static_members
 //ignore_for_file: unnecessary_brace_in_string_interps
