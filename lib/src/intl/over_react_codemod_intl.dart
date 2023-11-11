5a2ceb810dedb079be7c3265bc49f2adbde6a911
Dustin Pauze <dustin.pauze@workiva.com>
2023-11-11 08:09:53 -0500

    INTL-1692: wip

--- lib/src/intl/over_react_codemod_intl.dart
+++ lib/src/intl/over_react_codemod_intl.dart
@@ -3,29 +3,40 @@ import 'package:w_intl/intl_wrapper.dart';
 // This class is generated and maintained by the over_react_codemod:intl_message_migration.
 // It is possible to make manual changes to it, but be aware that it will be read and regenerated
 // any time the codemod is run, which will sort the members and apply its own formatting.
-// The type of code it may contain is limited to that which the codemod understands, and to 
+// The type of code it may contain is limited to that which the codemod understands, and to
 // things that can be extracted as Intl message. Limitations include:
 // - Only arrow functions
 // - Functions may only contain an Intl invocation
 // - Intl functions may only refer to variables in their parameter list and not to any external
 //   variables or functions.
 
-
 //ignore_for_file: avoid_classes_with_only_static_members
 //ignore_for_file: unnecessary_brace_in_string_interps
 class OverReactCodemodIntl {
-  static String get commonMessage => Intl.message('Check that `pub get` has been run and that this is a valid over_react component usage.', name: 'OverReactCodemodIntl_commonMessage');
+  static String get commonMessage => Intl.message(
+      'Check that `pub get` has been run and that this is a valid over_react component usage.',
+      name: 'OverReactCodemodIntl_commonMessage');
 
-  static String get fixmePrefix => Intl.message('FIXME:over_react_codemod', name: 'OverReactCodemodIntl_fixmePrefix');
+  static String get fixmePrefix => Intl.message('FIXME:over_react_codemod',
+      name: 'OverReactCodemodIntl_fixmePrefix');
 
-  static String get manualValidationCommentSubstring => Intl.message('Check this box upon manual', name: 'OverReactCodemodIntl_manualValidationCommentSubstring');
+  static String get manualValidationCommentSubstring =>
+      Intl.message('Check this box upon manual',
+          name: 'OverReactCodemodIntl_manualValidationCommentSubstring');
 
-  static String get outlineLinkButtonSkin => Intl.message('ButtonSkin.OUTLINE_LINK', name: 'OverReactCodemodIntl_outlineLinkButtonSkin');
+  static String get outlineLinkButtonSkin =>
+      Intl.message('ButtonSkin.OUTLINE_LINK',
+          name: 'OverReactCodemodIntl_outlineLinkButtonSkin');
 
-  static String get semverReportNotAvailable => Intl.message('Semver report not available; this class is assumed to be public and thus will not be updated.', name: 'OverReactCodemodIntl_semverReportNotAvailable');
+  static String get semverReportNotAvailable => Intl.message(
+      'Semver report not available; this class is assumed to be public and thus will not be updated.',
+      name: 'OverReactCodemodIntl_semverReportNotAvailable');
 
-  static String get temporaryCompanionClassComment => Intl.message('This will be removed once the transition to Dart 2 is complete.', name: 'OverReactCodemodIntl_temporaryCompanionClassComment');
+  static String get temporaryCompanionClassComment => Intl.message(
+      'This will be removed once the transition to Dart 2 is complete.',
+      name: 'OverReactCodemodIntl_temporaryCompanionClassComment');
 
-  static String get warning => Intl.message('Could not find semver_report.json.', name: 'OverReactCodemodIntl_warning');
-
+  static String get warning =>
+      Intl.message('Could not find semver_report.json.',
+          name: 'OverReactCodemodIntl_warning');
 }
