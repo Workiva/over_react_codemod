fa55d0654c7722208ea30ec590457de5e50e17a8
Aaron Lademann <aaron.lademann@workiva.com>
2024-04-10 13:34:35 -0600

    Add suggestor to migrate conditional prop function calls to null safety

---
+++ lib/src/dart3_suggestors/null_safety_prep/fn_prop_null_aware_call_suggestor.dart
@@ -0,0 +1,178 @@
+// Copyright 2024 Workiva Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import 'package:analyzer/dart/analysis/results.dart';
+import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/dart/ast/token.dart';
+import 'package:analyzer/dart/ast/visitor.dart';
+import 'package:analyzer/dart/element/type.dart';
+import 'package:collection/collection.dart';
+import 'package:over_react_codemod/src/util/class_suggestor.dart';
+
+/// Suggestor that replaces conditional calls to functions declared in props
+/// with inline null-aware property access.
+///
+/// This is helpful for null-safety migrations because the conditional
+/// function calls will otherwise get migrated with `!` modifiers.
+///
+/// **Before:**
+///
+/// ```dart
+/// if (props.someCallback != null) {
+///   props.someCallback(someValue);
+/// }
+///
+/// // Will be migrated to:
+/// if (props.someCallback != null) {
+///   props.someCallback!(someValue);
+/// }
+/// ```
+///
+/// **After:**
+///
+/// ```dart
+/// // This will require no changes during a null-safety migration.
+/// props.someCallback?.call(someValue);
+/// ```
+class FnPropNullAwareCallSuggestor extends RecursiveAstVisitor
+    with ClassSuggestor {
+  ResolvedUnitResult? _result;
+
+  @override
+  visitExpressionStatement(ExpressionStatement node) {
+    super.visitExpressionStatement(node);
+
+    if (node.expression is! BinaryExpression) return;
+
+    final relevantExprStatement =
+        _getPropFunctionExpressionBeingCalledConditionally(
+            node.expression as BinaryExpression);
+    final inlineBinaryExpr =
+        // This cast is safe due to the type checks within `_getPropFunctionExpressionBeingCalledConditionally`.
+        relevantExprStatement?.expression as BinaryExpression?;
+    if (inlineBinaryExpr == null) return;
+    final relevantFnExpr =
+        // This cast is safe due to the type checks within `_getPropFunctionExpressionBeingCalledConditionally`.
+        inlineBinaryExpr.rightOperand as FunctionExpressionInvocation;
+    // This cast is safe due to the type checks within `_getPropFunctionExpressionBeingCalledConditionally`.
+    final fn = relevantFnExpr.function as PropertyAccess;
+
+    yieldPatch(
+        '${fn.target}.${fn.propertyName}?.call${relevantFnExpr.argumentList};',
+        node.offset,
+        node.end);
+  }
+
+  @override
+  visitIfStatement(IfStatement node) {
+    super.visitIfStatement(node);
+
+    if (node.condition is! BinaryExpression) return;
+
+    final relevantFnExprStatement =
+        _getPropFunctionExpressionBeingCalledConditionally(
+            node.condition as BinaryExpression);
+    final relevantFnExpr =
+        // This cast is safe due to the type checks within `_getPropFunctionExpressionBeingCalledConditionally`.
+        relevantFnExprStatement?.expression as FunctionExpressionInvocation?;
+    if (relevantFnExpr == null) return;
+    // This cast is safe due to the type checks within `_getPropFunctionExpressionBeingCalledConditionally`.
+    final fn = relevantFnExpr.function as PropertyAccess?;
+    if (fn == null) return;
+
+    yieldPatch(
+        '${fn.target}.${fn.propertyName}?.call${relevantFnExpr.argumentList};',
+        node.offset,
+        node.end);
+  }
+
+  /// Returns the function expression (e.g. `props.onClick(event)`) being called
+  /// after the null condition is checked.
+  ExpressionStatement? _getPropFunctionExpressionBeingCalledConditionally(
+      BinaryExpression condition) {
+    // if (props.fn != null) { ... }
+    if (condition.parent is IfStatement) {
+      var propFunctionBeingNullChecked =
+          _getPropFunctionBeingNullChecked(condition);
+
+      return ((condition.parent! as IfStatement).thenStatement as Block)
+          .statements
+          .singleWhereOrNull((element) {
+        if (element is! ExpressionStatement) return false;
+        final expression = element.expression;
+        if (expression is! FunctionExpressionInvocation) return false;
+        final fn = expression.function;
+        if (fn is! PropertyAccess) return false;
+        final target = fn.target;
+        if (target is! SimpleIdentifier) return false;
+        if (target.name != 'props') return false;
+        final matches = fn.propertyName.staticElement?.declaration ==
+            propFunctionBeingNullChecked?.staticElement?.declaration;
+        return matches;
+      }) as ExpressionStatement?;
+      // props.fn != null && ...
+    } else if (condition.parent is ExpressionStatement &&
+        condition.leftOperand is BinaryExpression) {
+      final propFunctionBeingNullChecked = _getPropFunctionBeingNullChecked(
+          condition.leftOperand as BinaryExpression);
+      if (propFunctionBeingNullChecked == null) return null;
+
+      if (condition.rightOperand is! FunctionExpressionInvocation) return null;
+      final fn =
+          (condition.rightOperand as FunctionExpressionInvocation).function;
+      if (fn is! PropertyAccess) return null;
+      final target = fn.target;
+      if (target is! SimpleIdentifier) return null;
+      if (target.name != 'props') return null;
+      final matches = fn.propertyName.staticElement?.declaration ==
+          propFunctionBeingNullChecked.staticElement?.declaration;
+      if (!matches) return null;
+      return condition.parent as ExpressionStatement?;
+    }
+
+    return null;
+  }
+
+  /// Returns the identifier for the function that is being
+  /// null checked before being called.
+  SimpleIdentifier? _getPropFunctionBeingNullChecked(
+      BinaryExpression condition) {
+    if (condition.leftOperand is! PrefixedIdentifier) {
+      return null;
+    }
+    final leftOperand = condition.leftOperand as PrefixedIdentifier;
+    final prefix = leftOperand.prefix;
+    if (prefix.name != 'props') {
+      return null;
+    }
+    if (leftOperand.identifier.staticType is! FunctionType) {
+      return null;
+    }
+    if (condition.operator.stringValue != '!=' &&
+        condition.operator.next?.keyword != Keyword.NULL) {
+      return null;
+    }
+    return leftOperand.identifier;
+  }
+
+  @override
+  Future<void> generatePatches() async {
+    _result = await context.getResolvedUnit();
+    if (_result == null) {
+      throw Exception(
+          'Could not get resolved result for "${context.relativePath}"');
+    }
+    _result!.unit.accept(this);
+  }
+}
---
+++ test/dart3_suggestors/null_safety_prep/fn_prop_null_aware_call_suggestor_test.dart
@@ -0,0 +1,141 @@
+// Copyright 2024 Workiva Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import 'package:over_react_codemod/src/dart3_suggestors/null_safety_prep/fn_prop_null_aware_call_suggestor.dart';
+import 'package:test/test.dart';
+
+import '../../resolved_file_context.dart';
+import '../../util.dart';
+import '../../util/component_usage_migrator_test.dart';
+
+void main() {
+  final resolvedContext = SharedAnalysisContext.overReact;
+
+  // Warm up analysis in a setUpAll so that if getting the resolved AST times out
+  // (which is more common for the WSD context), it fails here instead of failing the first test.
+  setUpAll(resolvedContext.warmUpAnalysis);
+
+  group('FnPropNullAwareCallSuggestor', () {
+    late SuggestorTester testSuggestor;
+
+    setUp(() {
+      testSuggestor = getSuggestorTester(
+        FnPropNullAwareCallSuggestor(),
+        resolvedContext: resolvedContext,
+      );
+    });
+
+    group('handles block if conditions', () {
+      test('with a single condition', () async {
+        await testSuggestor(
+            expectedPatchCount: 1,
+            input: withOverReactImport('''
+              final Foo = uiFunction<UiProps>(
+                (props) {
+                  final handleClick = useCallback<MouseEventCallback>((e) {
+                    if (props.onClick != null) {
+                      props.onClick(e);
+                    }
+                  }, [props.onClick]);
+                  
+                  return (Dom.button()..onClick = handleClick)();
+                },
+                UiFactoryConfig(displayName: 'Foo'),
+              );
+            '''),
+            expectedOutput: withOverReactImport('''
+              final Foo = uiFunction<UiProps>(
+                (props) {
+                  final handleClick = useCallback<MouseEventCallback>((e) {
+                    props.onClick?.call(e);
+                  }, [props.onClick]);
+                  
+                  return (Dom.button()..onClick = handleClick)();
+                },
+                UiFactoryConfig(displayName: 'Foo'),
+              );
+            '''));
+      });
+
+      test('unless there are multiple conditions', () async {
+        await testSuggestor(
+          expectedPatchCount: 0,
+          input: withOverReactImport('''
+              final Foo = uiFunction<UiProps>(
+                (props) {
+                  final handleClick = useCallback<MouseEventCallback>((e) {
+                    if (props.onClick != null && props.onMouseEnter != null) {
+                      props.onClick(e);
+                    }
+                  }, [props.onClick]);
+                  
+                  return (Dom.button()..onClick = handleClick)();
+                },
+                UiFactoryConfig(displayName: 'Foo'),
+              );
+            '''),
+        );
+      });
+    });
+
+    group('handles inline if conditions', () {
+      test('with a single condition', () async {
+        await testSuggestor(
+            expectedPatchCount: 1,
+            input: withOverReactImport('''
+              final Foo = uiFunction<UiProps>(
+                (props) {
+                  final handleClick = useCallback<MouseEventCallback>((e) {
+                    props.onClick != null && props.onClick(e);
+                  }, [props.onClick]);
+                  
+                  return (Dom.button()..onClick = handleClick)();
+                },
+                UiFactoryConfig(displayName: 'Foo'),
+              );
+            '''),
+            expectedOutput: withOverReactImport('''
+              final Foo = uiFunction<UiProps>(
+                (props) {
+                  final handleClick = useCallback<MouseEventCallback>((e) {
+                    props.onClick?.call(e);
+                  }, [props.onClick]);
+                  
+                  return (Dom.button()..onClick = handleClick)();
+                },
+                UiFactoryConfig(displayName: 'Foo'),
+              );
+            '''));
+      });
+
+      test('unless there are multiple conditions', () async {
+        await testSuggestor(
+          expectedPatchCount: 0,
+          input: withOverReactImport('''
+              final Foo = uiFunction<UiProps>(
+                (props) {
+                  final handleClick = useCallback<MouseEventCallback>((e) {
+                    1 > 0 && props.onClick != null && props.onClick(e);
+                  }, [props.onClick]);
+                  
+                  return (Dom.button()..onClick = handleClick)();
+                },
+                UiFactoryConfig(displayName: 'Foo'),
+              );
+            '''),
+        );
+      });
+    });
+  });
+}
