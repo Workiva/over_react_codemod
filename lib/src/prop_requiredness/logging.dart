794f29eebd9b2c52a435ecf6ecdaba670f723384
Greg Littlefield <greg.littlefield@workiva.com>
2024-07-31 16:06:53 -0700

    Add coloring/formatting to logs

--- lib/src/prop_requiredness/logging.dart
+++ lib/src/prop_requiredness/logging.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:io/ansi.dart';
 import 'package:logging/logging.dart';
 
 /// Flag to help keep logs and progress output on separate lines.
@@ -15,7 +16,20 @@ void initLogging({bool verbose = false}) {
   Logger.root.onRecord.listen((record) {
     if (lastLogWasProgress) stderr.writeln();
     lastLogWasProgress = false;
-    print(verbose ? record : record.message);
+
+    AnsiCode color;
+    if (record.level < Level.WARNING) {
+      color = cyan;
+    } else if (record.level < Level.SEVERE) {
+      color = yellow;
+    } else {
+      color = red;
+    }
+    final message = StringBuffer()..write(color.wrap('[${record.level}] '));
+    if (verbose) message.write('${record.loggerName}: ');
+    message.write(record.message);
+    print(message.toString());
+
     if (record.error != null) print(record.error);
     if (record.stackTrace != null) print(record.stackTrace);
   });
