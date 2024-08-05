134e15cacc07582eb92f615497a6cdf55e00b768
Greg Littlefield <greg.littlefield@workiva.com>
2024-08-05 10:47:10 -0700

    Add copyright comments

--- lib/src/vendor/over_react_analyzer_plugin/get_all_props.dart
+++ lib/src/vendor/over_react_analyzer_plugin/get_all_props.dart
@@ -1,3 +1,17 @@
+// Copyright 2024 Workiva Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // Adapted from https://github.com/Workiva/over_react/blob/9079dff9405448ddaa5743a0ae408c4e0a056cec/tools/analyzer_plugin/lib/src/util/prop_declarations/get_all_props.dart
 
 import 'package:analyzer/dart/element/element.dart';
